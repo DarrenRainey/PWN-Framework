d6176011b750b240dca9f9c560cdfe43e20e13a6
Darren Rainey <darren@darrenraineys.co.uk>
2017-07-30 22:52:29 +0100

    Create updatebinary.c

---
+++ updatebinary.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define SERVER "127.0.0.1"
+
+void updatebinary()
+{
+  printf("============ Binary Update =========\n");
+  printf("Check For Updates From Server : %s\n",SERVER);
+}
